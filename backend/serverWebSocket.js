473bfbf2b79742264959c72413bd94a3e8c4638d
leonardo <leonardohuarayo@gmail.com>
2024-09-22 16:02:12 -0300

    listo chat completo

--- backend/serverWebSocket.js
+++ backend/serverWebSocket.js
@@ -1,34 +1,30 @@
+require('dotenv').config();
 const WebSocket = require('ws');
+const PORT = process.env.PORT || 4000;
 
-//iniciar servidor en el puerto 8080
-const wss = new WebSocket.Server({ port: 3000 });
+// Iniciar servidor en el puerto configurado
+const wss = new WebSocket.Server({ port: PORT });
 
-//cada vez que el cliente se conecte
+// Cada vez que el cliente se conecta
 wss.on('connection', (ws) => {
-  console.log('conexión WebSocket establecida')
+  console.log('Conexión WebSocket establecida');
 
-  //Escuchar mensajes del cliente
-  //Escucha mensajes que llegan a través de una conexión WebSocket de un cliente(que podría ser el Service Worker o cualquier otro cliente WebSocket)
-  wss.on('message', (event) => {
-    console.log('Mensaje recibido en el SW desde una pestaña: ', event.data);
-   
+  // Escuchar mensajes del cliente (cada conexión tiene su propio "ws")
+  ws.on('message', (message) => {
+    console.log('Mensaje recibido del cliente:', message);
+
     // Enviar el mensaje a todos los clientes conectados
     wss.clients.forEach((client) => {
-      console.log('readyState: ', client.readyState);
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(message); // Enviar el mensaje recibido a todos los clientes
       }
     });
-  
-  })
+  });
 
-  //cuando el cliente se desconecta
-  wss.on('close', () => {
+  // Manejar cierre de la conexión
+  ws.on('close', () => {
     console.log('Conexión WebSocket cerrada');
-  })
-  
-})
+  });
+});
 
-
-
-console.log('Servidor WebSocket escuchando en ws://localhost:3000')
+console.log(`Servidor WebSocket escuchando en ws://localhost:${PORT}`);
